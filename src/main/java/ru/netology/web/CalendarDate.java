4ab3440d0d230dd6c13150e701572bc3d07099aa
Anastasia Krapivina <krapivina934@gmail.com>
2023-06-29 11:19:35 +0700

    correction on commentsÐ

--- src/main/java/ru/netology/web/CalendarDate.java
+++ src/main/java/ru/netology/web/CalendarDate.java
@@ -6,16 +6,23 @@ import java.time.format.TextStyle;
 import java.util.Locale;
 
 public class CalendarDate {
-    public static void main(String[] args) {
+//        public static void main(String[] args) {
+//                        int days = 7;
+//                        String pattern = "dd MMMM yyyy";
+//        System.out.println(LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern(pattern, new Locale("RU"))));
+//    }
+//}
+   public static void main(String[] args) {
         LocalDate now = LocalDate.now(); // получаем текущую дату
         now = now.plusDays(7);
-        Locale localeRu = new Locale("ru", "RU");
-        String date = now.format(DateTimeFormatter.ofPattern("dd MMMM yyyy").withLocale(localeRu));
+        int month = now.getMonthValue();
+        int year = now.getYear();
+        String[] monthNames = {"Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"};
+        String monthEnd = monthNames[month-1];
 
-        System.out.println(date);
+        System.out.println(monthEnd+" "+ year);
     }
 }
-
 //.getDisplayName(TextStyle.FULL_STANDALONE, new Locale("ru"))
 //        String date = now.format(DateTimeFormatter.ofPattern("d"));
 //int dayOfMonth = now.getDayOfMonth();
--- src/test/java/ru/netology/web/CardDeliveryFormTest.java
+++ src/test/java/ru/netology/web/CardDeliveryFormTest.java
@@ -1,5 +1,6 @@
 package ru.netology.web;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 
@@ -14,6 +15,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
 import static com.codeborne.selenide.Condition.*;
+
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
@@ -24,30 +26,30 @@ public class CardDeliveryFormTest {
         open("http://localhost:9999");
     }
 
-    public String generate() { // вспомогательный метод к задаче 1
-        LocalDate now = LocalDate.now();
-        now = now.plusDays(4);
-        String date = now.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        return date;
+    public String generateDate(int days, String pattern) {  // вспомогательный метод к задаче 1
+
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern(pattern, new Locale("RU")));
     }
 
-    public String calendarDay() { // вспомогательный метод к задаче 2, генерирует выбор дня на неделю вперёд
-        LocalDate now = LocalDate.now(); // получаем текущую дату
-        now = now.plusDays(7);
-        String date = now.format(DateTimeFormatter.ofPattern("d"));
-        int dayOfMonth = now.getDayOfMonth();
-        int month = now.getMonthValue();
-        int year = now.getYear();
-        return date; // число
-    }
+//    public String calendarDay() { // вспомогательный метод к задаче 2, генерирует выбор дня на неделю вперёд
+//        LocalDate now = LocalDate.now(); // получаем текущую дату
+//        now = now.plusDays(7);
+//        String date = now.format(DateTimeFormatter.ofPattern("d"));
+//        int dayOfMonth = now.getDayOfMonth();
+//        int month = now.getMonthValue();
+//        int year = now.getYear();
+//        return date; // число
+//    }
 //   Вспомогательный метод к задаче 2,
 //   генерирует месяц и год для выбора даты на неделю вперед, не работает русская локаль.
     public String monthAndYear() {//
         LocalDate now = LocalDate.now(); // получаем текущую дату
         now = now.plusDays(7);
-        Locale localeRu = new Locale("ru", "RU");
-        String date = now.format(DateTimeFormatter.ofPattern("MMMM yyyy").withLocale(localeRu));
-        return date; // месяц и год
+        int month = now.getMonthValue();
+        int year = now.getYear();
+        String[] monthNames = {"Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"};
+        String monthEnd = monthNames[month-1];
+        return monthEnd+" "+ year; // месяц и год
     }
 
     @Test
@@ -58,12 +60,15 @@ public class CardDeliveryFormTest {
         form.$("[data-test-id=city] input").setValue("Новосибирск");
         form.$("[data-test-id=date] input").sendKeys(Keys.CONTROL + "A");
         form.$("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
-        form.$("[data-test-id=date] input").setValue(generate());
+        form.$("[data-test-id=date] input").setValue(generateDate(4, "dd.MM.yyyy"));
         form.$("[data-test-id=name] input").setValue("Иванов Иван");
         form.$("[data-test-id=phone] input").setValue("+79999990000");
         form.$("[data-test-id=agreement]").click();
         form.$(".button").click();
         $(withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + generateDate(4, "dd.MM.yyyy")), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
 
     }
 
@@ -77,17 +82,21 @@ public class CardDeliveryFormTest {
         form.$("[data-test-id=date] input").sendKeys(Keys.CONTROL + "A");
         form.$("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
         form.$("[data-test-id=date] .icon-button").click();
-        if ($(".calendar__name").has(text(monthAndYear()))) {
-            $$(".calendar__day").find(exactText(calendarDay())).click();
+        if ($(".calendar__name").has(exactText(monthAndYear()))) {
+            $$(".calendar__day").find(exactText(generateDate(7, "d"))).click();
         } else {
             $$(".calendar__arrow_direction_right").last().click();
-            $$(".calendar__day").find(exactText(calendarDay())).click();
+            $$(".calendar__day").find(exactText(generateDate(7, "d"))).click();
         }
         form.$("[data-test-id=name] input").setValue("Иванов Иван");
         form.$("[data-test-id=phone] input").setValue("+79999990000");
         form.$("[data-test-id=agreement]").click();
         form.$(".button").click();
         $(withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + generateDate(7, "dd.MM.yyyy")), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
+
     }
 
 
@@ -102,16 +111,19 @@ public class CardDeliveryFormTest {
         form.$("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
         form.$("[data-test-id=date] .icon-button").click();
         if ($(".calendar__name").has(text(monthAndYear()))) {
-            $$(".calendar__day").find(exactText(calendarDay())).click();
+            $$(".calendar__day").find(exactText(generateDate(7, "d"))).click();
         } else {
             $$(".calendar__arrow_direction_right").last().click();
-            $$(".calendar__day").find(exactText(calendarDay())).click();
+            $$(".calendar__day").find(exactText(generateDate(7, "d"))).click();
         }
         form.$("[data-test-id=name] input").setValue("Иванов Иван");
         form.$("[data-test-id=phone] input").setValue("+79999990000");
         form.$("[data-test-id=agreement]").click();
         form.$(".button").click();
         $(withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + generateDate(7, "dd.MM.yyyy")), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
     }
 
     @Test
@@ -125,16 +137,19 @@ public class CardDeliveryFormTest {
         form.$("[data-test-id=date] input").sendKeys(Keys.BACK_SPACE);
         form.$("[data-test-id=date] .icon-button").click();
         if ($(".calendar__name").has(text(monthAndYear()))) {
-            $$(".calendar__day").find(exactText(calendarDay())).click();
+            $$(".calendar__day").find(exactText(generateDate(7, "d"))).click();
         } else {
             $$(".calendar__arrow_direction_right").last().click();
-            $$(".calendar__day").find(exactText(calendarDay())).click();
+            $$(".calendar__day").find(exactText(generateDate(7, "d"))).click();
 }
             form.$("[data-test-id=name] input").setValue("Иванов Иван");
             form.$("[data-test-id=phone] input").setValue("+79999990000");
             form.$("[data-test-id=agreement]").click();
             form.$(".button").click();
             $(withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + generateDate(7, "dd.MM.yyyy")), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
         }
     }
 
